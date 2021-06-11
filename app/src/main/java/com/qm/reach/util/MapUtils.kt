b41dd29759f58f93db90cf5a8e7470d506178459
Qenawi <aldmanmero@yahoo.com>
2021-06-10 21:34:37 -0700

    added UnitTest for APi Service provider

---
+++ app/src/main/java/com/qm/reach/util/MapUtils.kt
@@ -0,0 +1,96 @@
+package com.qm.reach.util
+
+import com.google.gson.Gson
+import com.google.gson.JsonArray
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
+import com.google.gson.JsonSyntaxException
+import com.google.gson.reflect.TypeToken
+
+//MARK:- mapping util
+object MapUtils
+{
+  fun Map<String, Any>.mMapToJsonElement(): JsonElement = this.hashToJe()
+  private fun Map<String, Any>.hashToJe(): JsonElement {
+    val type2 = object : TypeToken<HashMap<String, Any>>() {}.type
+    val mToJsonParams: String = Gson().toJson(this, type2)
+    val ret = JsonParser().parse(mToJsonParams)
+    return ret
+  }
+
+  inline fun <reified T : Any> T.mAnyToJsonElement(): JsonElement {
+    val type2 = object : TypeToken<T>() {}.type
+    val mToJsonParams: String = Gson().toJson(this, type2)
+    return JsonParser().parse(mToJsonParams)
+  }
+
+  fun String.mStringToJsonElement(): JsonElement {
+    return JsonParser().parse(this)
+  }
+
+  @Throws(JsonSyntaxException::class)
+  inline fun <reified T : Any> JsonElement.mMapToArrayList(): ArrayList<T> =
+    try {
+      mapJson(this)
+    } catch (e: JsonSyntaxException) {
+      e.printStackTrace()
+      ArrayList()
+    }
+
+  @Throws(JsonSyntaxException::class)
+  inline fun <reified T : Any> JsonArray.mMapToArrayListFix(): ArrayList<T> =
+    try {
+      val ret = ArrayList<T>()
+      this.forEach { js ->
+        val obj: T? = try {
+          js.mMapToObject<T>()
+        } catch (e: Exception) {
+          e.printStackTrace()
+          null
+        }
+        obj?.let { ob ->
+          ret.add(ob)
+        }
+      }
+      ret
+    } catch (e: JsonSyntaxException) {
+      e.printStackTrace()
+      ArrayList()
+    }
+
+  inline fun <reified T : Any> JsonElement.mMapToObject(): T? =
+    try {
+      mapJson(this)
+    } catch (e: JsonSyntaxException) {
+      null
+    }
+
+  inline fun <reified T : Any> JsonObject.mMapToObject(): T? =
+    try {
+      mapJson(this)
+    } catch (e: JsonSyntaxException) {
+      null
+    }
+
+  @Throws(JsonSyntaxException::class)
+  inline fun <reified T : Any> mapJson(je: JsonElement): T {
+    val type = object : TypeToken<T>() {}.type
+    return Gson().fromJson(je, type)
+  }
+
+  @Throws(JsonSyntaxException::class)
+  inline fun <reified T : Any> mapJson(je: JsonObject): T {
+    val type = object : TypeToken<T>() {}.type//type
+    return Gson().fromJson(je, type)
+  }
+
+  fun ArrayList<*>?.validateArrayList() = this != null
+  fun String?.isValid() = !(this.isNullOrEmpty())
+  fun Any?.validateType() = this != null
+  fun isNotNull(A: Any?): Boolean {
+    return A != null
+  }
+
+  val emptyJe: JsonElement = HashMap<String, String>().mMapToJsonElement()
+}
--- app/src/test/java/com/qm/reach/home/HomeTest.kt
+++ app/src/test/java/com/qm/reach/home/HomeTest.kt
@@ -1,11 +1,56 @@
 package com.qm.reach.home
 
+import com.qm.reach.data.remote.ApiService
+import com.qm.reach.mock_server.MockServer
+import com.qm.reach.ui.fragment.home.model.CategoryRequestModel
+import com.qm.reach.ui.fragment.home.model.OffersGamesRequestModel
+import com.qm.reach.util.MapUtils.mMapToObject
+import com.qm.reach.util.MapUtils.mStringToJsonElement
+import io.reactivex.rxjava3.core.Flowable
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
 
+//MARK:- Api Service Mock
+class MocAPiService : ApiService {
+  override fun getTimeLine(): Flowable<OffersGamesRequestModel> {
+    val js = MockServer.sucessOffersJson
+    val res = js.mStringToJsonElement().asJsonObject.mMapToObject<OffersGamesRequestModel>()
+    return Flowable.just(res)
+  }
+
+  override fun getCategory(): Flowable<CategoryRequestModel> {
+    val js = MockServer.sucessCategoryJson
+    val res = js.mStringToJsonElement().asJsonObject.mMapToObject<CategoryRequestModel>()
+    return Flowable.just(res)
+  }
+}
+
+//MARK:- todo add Mockito (Application / viewModel)
 class HomeTest {
+  private lateinit var repo: ApiService
+
+  @Before
+  fun setup() {
+    repo = MocAPiService()
+  }
+
   @Test
   fun mainTestFunction() {
     Assert.assertEquals(4, 4)
+  }
+
+  @Test
+  fun parseOffers() {
+    var result: OffersGamesRequestModel? = null
+    repo.getTimeLine().subscribe { result = it }
+    Assert.assertNotNull(result)
+  }
+
+  @Test
+  fun parseCategories() {
+    var result: CategoryRequestModel? = null
+    repo.getCategory().subscribe { result = it }
+    Assert.assertNotNull(result)
   }
 }
